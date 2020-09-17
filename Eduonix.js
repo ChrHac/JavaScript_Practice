b4441a04c73d7ba7827291c0c3172e65a23c4cae
Chris <chrhac13@gmail.com>
2020-09-17 16:15:10 +0100

    first commit

---
+++ Eduonix.js
@@ -0,0 +1,32 @@
+//This file is notes that I have made during my completion of the "Advance JavaScript for Coders: Learn OOP in JavaScript" course on eduonix.com
+//All of the code will be commented out to avoid clutter on the console
+
+//Encapsulation is hiding the detail of functions when they are called
+
+//Object aggregation is using several smaller object to create one more complex object
+
+
+/*The below is a switch statement. THis will cycle through the cases until it finds one that evaluates as true or it will return the default case.
+Switch statements are used to replace complicated nests of for loops
+This switch will create an alert to say "It's Tuesday"*/
+
+
+let day = 2;
+
+switch (day) {
+    case 0:
+        alert("It's sunday");
+        break;
+    case 1:
+        alert("It's Monday");
+        break;
+    case 2: 
+        alert("It's Tuesday");
+        break;
+    default:
+        alert("I aint got a clue bro");
+        break;
+};
+
+
+
